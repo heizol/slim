dc90ab83e8ef5db92db1adb8ae027caadfe02b9c
heizol.shao@gmail.com <heizol.shao@gmail.com>
2016-10-18 18:46:46 +0800

    template

--- myslim/templates/default/index.php
+++ myslim/templates/default/index.php
@@ -1,1 +1,169 @@
-<?php
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+	<meta charset="utf-8">
+	<meta content="IE=edge" http-equiv="X-UA-Compatible">
+	<meta content="width=device-width,initial-scale=1" name="viewport">
+	<title>Bootstrap中文网开源项目免费 CDN 服务</title>
+	<meta content="Bootstrap中文网开源项目免费 CDN 服务 - 我们致力于为Bootstrap一样优秀的开源项目提供稳定、快速的免费 CDN 服务" name="description">
+	<meta content="" name="keywords">
+	<meta content="Bootstrap中文网" name="author">
+	<link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel="stylesheet" href="http://cdn.bootcss.com/font-awesome/4.3.0/css/font-awesome.css">
+	<link rel="stylesheet" href="http://www.bootcdn.cn/assets/css/site.min.css">
+	<!--[if lt IE 9]>
+    <script src="http://cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="http://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!--[if IE 9]>
+    <script src="http://cdn.bootcss.com/geopattern/1.2.3/js/base64.min.js"></script>
+    <script src="http://cdn.bootcss.com/geopattern/1.2.3/js/typedarray.min.js"></script>
+    <![endif]-->
+    <link href="http://www.bootcdn.cn/assets/ico/apple-touch-icon-144-precomposed.png" sizes="144x144" rel="apple-touch-icon-precomposed">
+    <link href="http://www.bootcdn.cn/assets/ico/favicon.ico" rel="shortcut icon">
+</head>
+<body class="home-template">
+	<!-- banner search -->
+	<header class="site-header jumbotron" style="background-image:none;">
+	   <div class="site-nav">
+	    <a href="#about">关于</a>
+	   </div>
+	   <div class="container">
+	    <div class="row">
+	     <div class="col-xs-12">
+	      <h1>BootCDN</h1>
+	      <p>稳定、快速、免费的开源项目 CDN 服务<br /><span class="package-amount">共收录了 <strong>2551</strong> 个开源项目</span></p>
+	      <form role="search" class="">
+	       <div class="form-group">
+	        <input type="text" placeholder="搜索开源库，例如：jquery" class="form-control search clearable" /> 
+	        <i class="fa x fa-close"></i>
+	       </div>
+	      </form>
+	     </div>
+	    </div>
+	   </div>
+   </header>
+   <!-- search -->
+   <div class="container protocal-notice">
+	   <div class="row">
+	    <div class="col-xs-12 hidden-xs">
+	     <form class="form-inline pull-right">
+	      <label>选择协议：</label>
+	      <div data-toggle="buttons" class="btn-group">
+	       <label class="btn btn-default active"><input type="radio" checked="" id="relative" name="options" />自适应</label>
+	       <label class="btn btn-default"><input type="radio" id="http" name="options" />http</label>
+	       <label class="btn btn-default"><input type="radio" id="https" name="options" />https</label>
+	      </div>
+	      <span class="help-block"><a onclick="_hmt.push(['_trackEvent', 'notice', 'click', 'protocal-notice'])" target="_blank" href="http://blog.bootcdn.cn/bootcdn-support-https/">了解详情 <i class="fa fa-info-circle"></i></a></span>
+	     </form>
+	    </div>
+	   </div>
+  </div>
+  <!-- main -->
+  <main id="all-packages" class="packages-list-container">
+	<div class="container">
+		<div class="list-group packages">
+			<!--  list -->
+			<a class="package list-group-item" target="_blank" data-library-name="bootstrap" href="/bootstrap/">
+			 <div class="row">
+			    <div class="col-md-3">
+			     <h4 class="package-name">bootstrap</h4>
+			    </div>
+			    <div class="col-md-9 hidden-xs">
+			     <p class="package-description">The most popular front-end framework for developing responsive, mobile first projects on the web.</p>
+			    </div>
+			    <div class="package-extra-info col-md-9 col-md-offset-3 col-xs-12">
+			     <span><i class="fa fa-star"></i> 101774</span>
+			    </div>
+			 </div>
+			 </a>
+			 <a class="package list-group-item" target="_blank" data-library-name="bootstrap" href="/bootstrap/">
+			 <div class="row">
+			    <div class="col-md-3">
+			     <h4 class="package-name">bootstrap</h4>
+			    </div>
+			    <div class="col-md-9 hidden-xs">
+			     <p class="package-description">The most popular front-end framework for developing responsive, mobile first projects on the web.</p>
+			    </div>
+			    <div class="package-extra-info col-md-9 col-md-offset-3 col-xs-12">
+			     <span><i class="fa fa-star"></i> 101774</span>
+			    </div>
+			 </div>
+			 </a>
+			 
+			 <a class="package list-group-item" target="_blank" data-library-name="bootstrap" href="/bootstrap/">
+			 <div class="row">
+			    <div class="col-md-3">
+			     <h4 class="package-name">bootstrap</h4>
+			    </div>
+			    <div class="col-md-9 hidden-xs">
+			     <p class="package-description">The most popular front-end framework for developing responsive, mobile first projects on the web.</p>
+			    </div>
+			    <div class="package-extra-info col-md-9 col-md-offset-3 col-xs-12">
+			     <span><i class="fa fa-star"></i> 101774</span>
+			    </div>
+			 </div>
+			 </a>
+			 
+		</div>
+	</div>
+  </main>
+  <!-- footer -->
+  <footer class="footer hidden-print" id="footer">
+   <div class="container">
+    <div class="row">
+     <div id="about" class="footer-about col-md-5 col-sm-12">
+      <h4>关于 BootCDN</h4>
+      <p>BootCDN 是 <a onclick="_hmt.push(['_trackEvent', 'footer', 'click', 'footer-bootcss.com'])" target="_blank" href="http://www.bootcss.com/">Bootstrap中文网</a>支持并维护的开源项目免费 CDN 服务，致力于为 Bootstrap、jQuery、Angular 一样优秀的开源项目提供稳定、快速的免费 CDN 服务。BootCDN 所收录的开源项目主要同步于 <a onclick="_hmt.push(['_trackEvent', 'footer', 'click', 'footer-github.com'])" target="_blank" href="https://github.com/cdnjs/cdnjs">cdnjs</a> 仓库。</p>
+      <p>自2013年10月31日上线以来已经为近万家网站提供了稳定、可靠的 CDN 服务。</p>
+      <p>反馈或建议请发送邮件至：cdn@bootcss.com</p>
+     </div>
+     <div class="footer-links col-md-2 col-sm-12">
+      <h4>友情链接</h4>
+      <ul class="list-unstyled">
+       <li><a onclick="_hmt.push(['_trackEvent', 'footer', 'click', 'footer-bootcss.com'])" target="_blank" href="http://www.bootcss.com/">Bootstrap 中文网</a></li>
+       <li><a onclick="_hmt.push(['_trackEvent', 'footer', 'click', 'footer-ghostchina.com'])" target="_blank" href="http://www.ghostchina.com/">Ghost 中国</a></li>
+       <li><a onclick="_hmt.push(['_trackEvent', 'footer', 'click', 'footer-golaravel.com'])" target="_blank" href="http://www.golaravel.com/">Laravel 中文网</a></li>
+       <li><a onclick="_hmt.push(['_trackEvent', 'footer', 'click', 'footer-jquery123.com'])" target="_blank" href="http://www.jquery123.com/">jQuery 中文 API</a></li>
+       <li><a onclick="_hmt.push(['_trackEvent', 'footer', 'click', 'footer-phpcomposer.com'])" target="_blank" href="http://www.phpcomposer.com/">Composer 中国镜像</a></li>
+      </ul>
+     </div>
+     <div class="footer-techs col-md-3 col-sm-12">
+      <h4>我们用到的技术</h4>
+      <ul class="list-unstyled list-inline">
+       <li><a onclick="_hmt.push(['_trackEvent', 'footer', 'click', 'footer-bootcss.com'])" target="_blank" href="http://www.bootcss.com/">Bootstrap</a></li>
+       <li><a onclick="_hmt.push(['_trackEvent', 'footer', 'click', 'footer-ghostchina.com'])" target="_blank" href="http://www.ghostchina.com/">Ghost</a></li>
+       <li><a onclick="_hmt.push(['_trackEvent', 'footer', 'click', 'footer-jquery123.com'])" target="_blank" href="http://www.jquery123.com/">jQuery</a></li>
+       <li><a onclick="_hmt.push(['_trackEvent', 'footer', 'click', 'footer-babeljs.cn'])" target="_blank" href="http://babeljs.cn/">Babeljs</a></li>
+       <li><a onclick="_hmt.push(['_trackEvent', 'footer', 'click', 'footer-lodashjs.com'])" target="_blank" href="http://lodashjs.com/">Lodash</a></li>
+       <li><a onclick="_hmt.push(['_trackEvent', 'footer', 'click', 'footer-nodeapp.cn'])" target="_blank" href="http://www.nodeapp.cn/">Node</a></li>
+       <li><a onclick="_hmt.push(['_trackEvent', 'footer', 'click', 'footer-gruntjs.net'])" target="_blank" href="http://www.gruntjs.net/">Grunt</a></li>
+       <li><a onclick="_hmt.push(['_trackEvent', 'footer', 'click', 'footer-gulpjs.com.cn'])" target="_blank" href="http://www.gulpjs.com.cn/">Gulp</a></li>
+       <li><a onclick="_hmt.push(['_trackEvent', 'footer', 'click', 'footer-npmjs.com.cn'])" target="_blank" href="http://www.npmjs.com.cn/">NPM</a></li>
+      </ul>
+     </div>
+     <div class="footer-sponsors col-md-2 col-sm-12">
+      <h4>赞助</h4>
+      <p></p>
+     </div>
+    </div>
+   </div>
+   <div class="copy-right">
+    <span>&copy; 2013-2016</span> 
+    <a target="_blank" href="http://www.miibeian.gov.cn/">京ICP备11008151号</a> 
+    <span>京公网安备11010802014853</span>
+   </div>
+  </footer>
+    <script src="//cdn.bootcss.com/jquery/3.0.0/jquery.min.js"></script>
+	<script src="//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<script src="//cdn.bootcss.com/geopattern/1.2.3/js/geopattern.min.js"></script>
+	<script src="//cdn.bootcss.com/zeroclipboard/2.2.0/ZeroClipboard.min.js"></script>
+	<script src="//cdn.bootcss.com/localforage/1.4.2/localforage.min.js"></script>
+	<script src="http://www.bootcdn.cn/assets/js/site.min.js"></script>
+	<script type="text/javascript">
+	$(document).ready(function(){
+		$(".site-header").attr("style", "");
+	});
+	</script>
+</body>
+</html>
